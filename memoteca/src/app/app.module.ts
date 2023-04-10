b03bd6954766615399eb9e97e16806ff28bba905
rldcarvalho <rldcarvalhoo@gmail.com>
2023-04-10 18:32:15 -0300

    Adicionado componente cabeçalho

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
