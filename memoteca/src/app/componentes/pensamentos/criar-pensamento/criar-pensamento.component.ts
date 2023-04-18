55494c8e3fb1920354655791894fa86d8a1e224f
rldcarvalho <rldcarvalhoo@gmail.com>
2023-04-18 18:23:53 -0300

    Adicionado required validator no formulario

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -20,9 +20,12 @@ export class CriarPensamentoComponent {
   formulario!: FormGroup;
 
   criarPensamento(){
-    this.service.criar(this.formulario.value).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario)
+    if(this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelar() {
@@ -31,8 +34,8 @@ export class CriarPensamentoComponent {
 
   ngOnInit(): void{
     this.formulario = this.formBuilder.group({
-      conteudo: ['Formulario Reativo'],
-      autoria: ['Angular'],
+      conteudo: ['', [Validators.required]],
+      autoria: ['', [Validators.required]],
       modelo: ['modelo1']
 
     })
