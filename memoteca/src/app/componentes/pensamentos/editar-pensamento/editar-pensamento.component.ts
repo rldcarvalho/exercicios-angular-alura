7501f2e8c405523034ebb9d430d6f8f2503c8abe
rldcarvalho <rldcarvalhoo@gmail.com>
2023-04-20 17:51:23 -0300

    Adicionado FormGroup/Validators editar pensamento

--- memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PensamentoService } from '../pensamento.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -10,33 +11,49 @@ import { PensamentoService } from '../pensamento.service';
 })
 export class EditarPensamentoComponent {
 
-  pensamento: Pensamento = {
-    id: 0,
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
-
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { }
 
+  formulario!: FormGroup;
+
   editarPensamento(){
-    this.service.editar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
-    })
+    if(this.formulario.valid){
+      this.service.editar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelar(){
     this.router.navigate(['/listarPensamento'])
   }
 
+  habilitarBotao(): string{
+    if(this.formulario.valid){
+      return 'botao'
+    }
+    return 'botao__desabilitado'
+  }
+
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
-        this.pensamento = pensamento
+        this.formulario = this.formBuilder.group({
+          id: [pensamento.id],
+          conteudo: [pensamento.conteudo, Validators.compose([
+            Validators.required,
+            Validators.pattern(/(.|\s)*\S(.|\s)*/)
+          ])],
+          autoria: [pensamento.autoria, Validators.compose([
+            Validators.required,
+            Validators.minLength(3)])
+          ],
+          modelo: [pensamento.modelo]
+        })
     })
   }
 }
