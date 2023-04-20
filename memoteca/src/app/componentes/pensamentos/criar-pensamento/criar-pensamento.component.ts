f529fb933a1e1545d0c799731b2e2b5a311661e8
rldcarvalho <rldcarvalhoo@gmail.com>
2023-04-20 16:40:09 -0300

    Adicionada validação customizada

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,6 +3,7 @@ import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculoValidator';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -47,7 +48,8 @@ export class CriarPensamentoComponent {
       ])],
       autoria: ['', Validators.compose([
         Validators.required,
-        Validators.minLength(3)
+        Validators.minLength(3),
+        minusculoValidator
       ])],
       modelo: ['modelo1']
 
