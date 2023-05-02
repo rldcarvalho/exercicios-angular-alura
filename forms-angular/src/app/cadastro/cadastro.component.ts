52ee8f560f0da8c3c456486f317d91ae2db406fe
rldcarvalho <rldcarvalhoo@gmail.com>
2023-05-02 17:16:29 -0300

    Adicionado service da API Viacep

--- forms-angular/src/app/cadastro/cadastro.component.ts
+++ forms-angular/src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ConsultaCepService } from '../service/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,7 +10,7 @@ import { NgForm } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private consultaCepService: ConsultaCepService) { }
 
   ngOnInit(): void {
   }
@@ -23,5 +24,10 @@ export class CadastroComponent implements OnInit {
     }
     
     console.log(form.controls)
+  }
+
+  consultaCEP(ev: any) {
+    const cep = ev.target.value;
+    return this.consultaCepService.getConsultaCep(cep).subscribe(resultado => console.log(resultado))
   }
 }
