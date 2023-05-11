723af5599a6c561b0b505643a2ff410eb95e98b7
rldcarvalho <rldcarvalhoo@gmail.com>
2023-05-11 19:47:06 -0300

    add feat pokemon data

--- projetos/pokedex/src/app/components/pokemon-card/pokemon-card.component.ts
+++ projetos/pokedex/src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/model/Interfaces/pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-card.component.sass']
 })
 export class PokemonCardComponent {
+  @Input()
+  public pokemon!: Pokemon;
 
+  leadingZero(str: string | number, size: number = 3): string {
+    let s = String(str);
+
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+
+    return s;
+  }
 }
--- projetos/pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
+++ projetos/pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pokemon } from 'src/model/Interfaces/pokemon';
+import { Type } from 'src/model/enums/type';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-list.component.sass']
 })
 export class PokemonListComponent {
-
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types: [
+        Type.GRASS,
+        Type.POISON
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      number: 2,
+      name: 'Ivysaur',
+      types: [
+        Type.GRASS,
+        Type.POISON
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+      number: 3,
+      name: 'Venosaur',
+      types: [
+        Type.GRASS,
+        Type.POISON
+      ]
+    }
+  ];
 }
